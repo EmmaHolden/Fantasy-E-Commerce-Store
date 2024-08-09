4f7557d22337e940d467118f266a3a02b5ccfae5
Emma Holden <emmaholden91@yahoo.com>
2024-08-09 22:51:12 +0100

    Added news items for noticeboard

---
+++ frontend/src/newsItemsArray.js
@@ -0,0 +1,18 @@
+let itemList = [
+
+    ["HEAR YE! HEAR YE!", "Our King has increased the taxes by 45% for the coming season. Join us in the revolution lest you starve in your beds!"],
+    ["CHAMPIONS WANTED!", "The Grappling Hook Arena is holding a tournament at sunset. Combatant spaces still available. Only the most courageous need apply."],
+    ["OPEN AUDITIONS!", "Travelling entertainment troupe seeking new members. Auditions at the Raven's Nook on the night of the new moon."],
+    ["CUSTOMER ANNOUNCEMENT!", "The Lonely Asp Parlour would like to inform all customers that payment for drinks and services is now required upfront."],
+    ["JOB OPPORTUNITY!", "Experienced tavernkeeper seeking apprentice staff. Room and meals as payment. Ask for Voytarn at the Jackal and Drake Inn."],
+    ["MAN SEEKING MAIDEN!", "To the young maiden travelling west through Mistyvale, I cannot cease thinking of you. If seeking a suitor, please contact the Eaglesburg house."],
+    ["PUBLIC SAFETY WARNING!", "Griffon spotted over Gnarly Point Valley. Proceed through this area with extreme caution and report any sightings immediately."],
+    ["SESSIONS CANCELLED!", "Due to lack of interest, the Frost Spider taming sessions for children will no longer be running. Refunds to be given in due course."],
+    ["RANGER SERVICES FOR SALE!", "Selling tracking, hunting and marksman services for a very reasonable price. Find Rylark at the Ranger's Guild for more information."],
+    ["SEEKING DONATIONS!", "Collecting donations for the Tomewyrm Library. Accepting all spellbooks, tomes, journals, scribe supplies and monetary donations."],
+    ["PET FOUND!", "Lava lizard roaming around the Caltrop Stables. Appears tame but is scaring the horses. Owner to collect before Snowfall or it will be destroyed."],
+    ["THE END IS NIGH!", "Open your eyes and embrace the truth. The Followers of the Dark Night meet every sunset in the Seaport Tavern. Ale provided for a small fee."],
+    ["ENCHANTER SERVICES AVAILABLE!", "Enchanting and imbuing services provided in the Blackfall Mage's Tower. Upon arrival, please do not directly acknowledge any wandering familiars."]
+]
+
+export default itemList;
